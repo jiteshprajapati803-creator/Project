5122250eedc36f81e74e4f9166b9a12d51942862
Jitendra834 <jituprajapati834@gmail.com>
2026-05-23 15:40:26 +0530

    a

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Qick_Fix_WebAPI.Controllers
     {
         private static readonly string[] Summaries =
         [
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","more"
         ];
 
         [HttpGet(Name = "GetWeatherForecast")]
